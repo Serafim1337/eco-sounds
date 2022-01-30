75deef1e47f93ec4af2b88a390e08548b9d72332
Serafim <7620226@gmail.com>
2022-01-31 02:12:17 +0300

    feat: add audio and favicon

--- script.js
+++ script.js
@@ -1,68 +1,114 @@
 (function () {
-  const backgrounds = document.querySelectorAll('.background');
-  let counter = 0; 
+  const backgrounds = document.querySelectorAll(".background");
+  let counter = 0;
   backgrounds.forEach((bg) => {
     counter++;
     let image = new Image();
-    image.src = `assets/img/${counter}.jpg`
-  })
+    image.src = `assets/img/${counter}.jpg`;
+  });
 })(); //!backgrounds preload
 
-const images = document.querySelectorAll('.bird-images')
+//!------------------------- changing bg functions
+
+const images = document.querySelectorAll(".bird-images");
 images.forEach((image) => {
-  image.addEventListener('click', imagesHandler);
-  image.addEventListener('click', imageActivityHandler);
-})
+  image.addEventListener("click", imagesHandler);
+  image.addEventListener("click", imageActivityHandler);
+});
 
-const logo = document.querySelector('.logo');
-logo.addEventListener('click', imagesHandler);
-logo.addEventListener('click', unableImagesActivity)
+const logo = document.querySelector(".logo");
+logo.addEventListener("click", imagesHandler);
+logo.addEventListener("click", unableImagesActivity);
 
-function imagesHandler (event) {
-changeBg(event);
+function imagesHandler(event) {
+  changeBg(event);
 }
 
 function imageActivityHandler(event) {
-  event.target.classList.add('active');
+  event.target.classList.add("active");
   images.forEach((image) => {
-    if(image !== event.target) {
-      image.classList.remove('active');
+    if (image !== event.target) {
+      image.classList.remove("active");
     }
-  })
+  });
 }
 
-function changeBg (event) {
-const backgrounds = document.querySelectorAll('.background'); 
-const currentBgId = event.target.dataset.bg;
-const currentBg = document.querySelector(`#${currentBgId}`);
-currentBg.style.display = 'block';
-backgrounds.forEach((bg) => {
-  if(bg.id !== currentBgId) {
-    bg.style.display = 'none';
+function changeBg(event) {
+  const backgrounds = document.querySelectorAll(".background");
+  const currentBgId = event.target.dataset.bg;
+  const currentBg = document.querySelector(`#${currentBgId}`);
+  currentBg.style.display = "block";
+  backgrounds.forEach((bg) => {
+    if (bg.id !== currentBgId) {
+      bg.style.display = "none";
+    }
+  });
+  if (isPaused()) {
+    operateAudio();
   }
-})
 }
 
 function unableImagesActivity() {
   images.forEach((image) => {
-      image.classList.remove('active');
-  })
+    image.classList.remove("active");
+  });
 }
 
-//!----play button
-// (function() {
-//   var btn = document.querySelector(".button");
-//   btn.click(function() {
-//     btn.toggleClass("paused");
-//     return false;
-//   });
-// })(); 
+//!---------------------------- play button
 
 const btn = document.querySelector(".button");
-btn.addEventListener('click', btnHandler);
+btn.addEventListener("click", btnHandler);
+btn.addEventListener("click", operateAudio);
 
-function btnHandler (event) {
-  event.target.classList.toggle('paused')
+function btnHandler(event) {
+  event.target.classList.toggle("paused");
 }
 
+function isPaused() {
+  return btn.classList.contains("paused") ? true : false;
+}
 
+//!----------------------------- audio functions
+
+const audio = new Audio();
+document.querySelector("#bg-1").style.display = "block";
+
+function operateAudio() {
+  let currentBgId = "";
+
+  if (document.querySelector("#bg-1").style.display === "block") {
+    currentBgId = "bg-1";
+  } else {
+    currentBgId = document.querySelector('[style="display: block;"]').id;
+  }
+  switch (currentBgId) {
+    case "bg-1":
+      audio.src = "assets/audio/forest.mp3";
+      break;
+    case "bg-2":
+      audio.src = "assets/audio/drozd.mp3";
+      break;
+    case "bg-3":
+      audio.src = "assets/audio/javoronok.mp3";
+      break;
+    case "bg-4":
+      audio.src = "assets/audio/slavka.mp3";
+      break;
+    case "bg-5":
+      audio.src = "assets/audio/solovey.mp3";
+      break;
+    case "bg-6":
+      audio.src = "assets/audio/zaryanka.mp3";
+      break;
+  }
+
+  switch (isPaused()) {
+    case true:
+      audio.volume = 0.1;
+      audio.play();
+      break;
+    case false:
+      audio.pause();
+      break;
+  }
+}
